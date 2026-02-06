03fa320fad27b66660c79aa165f68f32c6a067dd
Ubuntu <ubuntu@vps-d3ab247a.vps.ovh.us>
2026-02-06 01:01:51 -0600

    fix(nlu): export current intent helpers
    bd-u88
    
    bd-qrv

--- src/infobot/nlu/__init__.py
+++ src/infobot/nlu/__init__.py
@@ -1,1 +1,16 @@
 """Natural language understanding for intent detection and question parsing."""
+
+from infobot.nlu.factoid_intent_detector import (
+    FactoidCreateIntent,
+    ModificationType,
+    detect_factoid_create_intent,
+)
+from infobot.nlu.question_parser import QuestionIntent, parse_question
+
+__all__ = [
+    "FactoidCreateIntent",
+    "ModificationType",
+    "QuestionIntent",
+    "detect_factoid_create_intent",
+    "parse_question",
+]
